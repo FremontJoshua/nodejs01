2224f846f5f588cd22bafdb98c54db22d40a4c90
FremontJoshua <fremont.joshua85@gmail.com>
2018-01-16 09:22:19 +0100

    starting babel

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log('Test');
-
+let toto = 8;
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,12 @@ module.exports = {
     entry: './index.js',
     output: {
         filename: 'dist/bundle.js',
-    }
+    },
+    module: {
+        loaders: [
+          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+        ]
+      }
+
 };
 
